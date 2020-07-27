1df5e0c722c5dfd309579f63936179f491c61fec
Konstantinos Takakis <ktakakis@live.com>
2020-07-28 01:26:32 +0300

    Add project files.

---
+++ kipopraxis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kipopraxis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
